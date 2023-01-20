46d632650586efd8ee8bdb54fc36ff98a7e80bb7
Ian Leong <ianleong726@gmail.com>
2023-01-20 16:35:39 +0800

    change value of row and column

--- game.cpp
+++ game.cpp
@@ -1,14 +1,3 @@
-// ********************************************************* 
-// Course: TCP1101 PROGRAMMING FUNDAMENTALS 
-// Year: Trimester 1, 2022/23 (T2215) 
-// Lab: TxxL 
-// Names: MEMBER_NAME_1 | MEMBER_NAME_2 | MEMBER_NAME_3 
-// IDs: MEMBER_ID_1 | MEMBER_ID_2 | MEMBER_ID_3 
-// Emails: MEMBER_EMAIL_1 | MEMBER_EMAIL_2 | MEMBER_EMAIL_3 
-// Phones: MEMBER_PHONE_1 | MEMBER_PHONE_2 | MEMBER_PHONE_3 
-// ********************************************************* 
-
-// TODO: Fill in the missing information above and delete this line.
 #include <vector>
 #include <cstdlib>
 #include <iostream>
@@ -93,9 +82,9 @@ void GameSettings(int& rows, int& columns)
         CreateBoard(rows, columns);
         ShowGameBoard(rows, columns);
     }
-    else if (yesorno == 'n')
-    
+    else if (yesorno == 'n')   
     {
+        board.resize(rows);
         for (int i = 0; i < rows; ++i)
         {
           board[i].resize(columns);
