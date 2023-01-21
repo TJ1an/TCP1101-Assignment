ba8e3e8f05897b9ff419104627972f9120ca520e
Ian Leong <ianleong726@gmail.com>
2023-01-21 22:09:27 +0800

    changes to main

--- main.cpp
+++ main.cpp
@@ -9,113 +9,12 @@
 // ********************************************************* 
 
 // TODO: Fill in the missing information above and delete this line.
+#include <board.cpp>
 #include <vector>
 #include <cstdlib>
 #include <iostream>
 using namespace std;
 
-<<<<<<< HEAD
-vector< vector<char> > board;
-
-int ClearScreen()
-{
-#if defined(_WIN32)
-    return std::system("cls");
-#elif defined(__linux__) || defined(__APPLE__)
-    return std::system("clear");
-#endif
-}
-
-int Pause()
-{
-#if defined(_WIN32)
-    return std::system("pause");
-#elif defined(__linux__) || defined(__APPLE__)
-    return std::system(R"(read -p "Press any key to continue . . . " dummy)");
-#endif
-}
-
-void CreateBoard(int rows,int columns)
-{
-  srand((unsigned) time(NULL));
-    for (int row = 0; row < rows; ++row)
-        for (int col = 0; col < columns; ++col)
-        {
-            int random_number = rand() % 2;
-            if (random_number){
-                board[row][col] = '*';
-            }
-            else{
-                board[row][col] = 'r';
-            }
-        }
-}
-
-void ShowGameBoard(int rows, int columns)
-    {
-        for (int row = 0; row < rows; ++row)
-        {
-            for (int col = 0; col < columns; ++col)
-            {
-                std::cout << board[row][col];
-            }
-            std::cout << std::endl;
-        }
-
-            
-    }
-
-void GameSettings(int& rows, int& columns)
-{
-    char yesorno;
-    std::cout << "Default game setttings  \n"
-              << std::endl;
-    std::cout << "---------------------- \n"
-              << std::endl;
-    std::cout << "board rows : " << rows << std::endl;
-    std::cout << "board columns : " << columns << std::endl;
-    std::cout << " " << std::endl;
-    std::cout << "do you want to change the game settings? (y/n) => ";
-    std::cin >> yesorno;
-
-    if (yesorno == 'y')
-    {
-        int newRows;
-        int newColumns;
-        ClearScreen();
-        std::cout << "board rows : ";
-        std::cin >> newRows;
-        std::cout << "board columns : ";
-        std::cin >> newColumns;
-        rows = newRows;
-        columns = newColumns;
-        board.resize(rows);
-        for (int i = 0; i < rows; ++i)
-        {
-          board[i].resize(columns);
-        }
-        CreateBoard(rows, columns);
-        ShowGameBoard(rows, columns);
-    }
-    else if (yesorno == 'n')
-    
-    {
-        board.resize(rows);
-        for (int i = 0; i < rows; ++i)
-        {
-          board[i].resize(columns);
-        }
-        CreateBoard(5, 5);
-        ShowGameBoard(5, 5);
-    }
-    else
-    {
-        std::cout << "bruh" << std::endl;
-    }
-}
-
-=======
->>>>>>> e2daddaf161b78f6f6a0ee758bc58ac4eb106633
 int main()
 {
     int kRows = 5;
