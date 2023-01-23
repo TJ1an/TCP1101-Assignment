787ea90c7e5df0ac0fb056c80c8afbe53db212b2
Anglified <trueanglified@gmail.com>
2023-01-23 10:33:14 +0800

    Update alien.cpp
    Fixed 3 bugs: Alien health being ramdomised when shouldn't, Alien health randomising after each round, "Invalid Move" shows up after every round

--- alien.cpp
+++ alien.cpp
@@ -6,70 +6,81 @@ using namespace std;
 
 extern vector< vector<char> > board;
 
-void alienStats()
-{
-    srand((unsigned) time(NULL)); 
-    int alienHealth[]={50,100,150,200,250};
-    int randomAlienHealth = rand() % size(alienHealth);
-    cout << "Alien Health: ";
-    cout << alienHealth[randomAlienHealth] << endl;
-}
-
+int alienHealth = 100;
 
 void MoveAlien(int &x, int &y, int rows, int columns) {
     string direction;
-    alienStats();
+    cout << "Your Health: " << alienHealth << endl;
     cout << "Enter the direction you want to move the alien (up, down, left, right): ";
     cin >> direction;
-    if (direction == "up") {
+    if (direction == "up")
+    {
         while(true)
-        if (x - 1 >= 0 && board[x - 1][y] != 'r') {
-            board[x][y] = '.';
-            x--;
-            board[x][y] = 'A';
-        }
-        else {
-            cout << "Invalid move" << endl;
-        break;
+        {
+            if (x - 1 >= 0 && board[x - 1][y] != 'r')
+            {
+                board[x][y] = '.';
+                x--;
+                board[x][y] = 'A';
+            }
+            else
+            {
+                cout << "Invalid move" << endl;
+                break;
+            }
         }
     }
     else if (direction == "down") {
         while(true)
-        if (x + 1 < rows && board[x + 1][y] != 'r') {
-            board[x][y] = '.';
-            x++;
-            board[x][y] = 'A';
-        }
-        else {
-            cout << "Invalid move" << endl;
-        break;
+        {
+            if (x + 1 < rows && board[x + 1][y] != 'r')
+            {
+                board[x][y] = '.';
+                x++;
+                board[x][y] = 'A';
+            }
+            else
+            {
+                cout << "Invalid move" << endl;
+                break;
+            }
         }
     }
     else if (direction == "left") {
         while(true)
-        if (y - 1 >= 0 && board[x][y - 1] != 'r') {
-            board[x][y] = '.';
-            y--;
-            board[x][y] = 'A';
-        }
-        else {
-            cout << "Invalid move" << endl;
-        break;
+        {
+            if (y - 1 >= 0 && board[x][y - 1] != 'r')
+            {
+                board[x][y] = '.';
+                y--;
+                board[x][y] = 'A';
+            }
+            else
+            {
+                cout << "Invalid move" << endl;
+            break;
+            }
         }
     }
-    else if (direction == "right") {
+    else if (direction == "right")
+    {
         while(true)
-        if (y + 1 < columns && board[x][y + 1] != 'r') {
-            board[x][y] = '.';
-            y++;
-            board[x][y] = 'A';
-        }
-        else {
-            cout << "Invalid move" << endl;
-        break;
+        {
+            if (y + 1 < columns && board[x][y + 1] != 'r')
+            {
+                board[x][y] = '.';
+                y++;
+                board[x][y] = 'A';
+            }
+            else
+            {
+                cout << "Invalid move" << endl;
+            break;
+            }
         }
     }
-    else {
+    else
+    {
         cout << "Invalid input" << endl;
     }
 }
